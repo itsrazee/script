399651439f8ee298fc31fc6c5b931edb6dd09404
imjustachild <ngkhanggg1572@gmail.com>
2026-02-27 23:22:13 +0700

    Load scripts based on game PlaceId

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 2116452049 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/itsrazee/script/main/sop.lua"))()
+elseif game.PlaceId == 112233665771826 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/itsrazee/script/main/opd.lua"))()
+end
